a70bdccdf8259c6f9044f0a287c0bf8de9f35ad7
Tim Cera <tim@cerazone.net>
2025-10-05 13:02:25 -0400

    fix: correctly add support for python-rstansi

--- src/cltoolbox/__init__.py
+++ src/cltoolbox/__init__.py
@@ -1,3 +1,6 @@
+import os
+import sys
+
 from cltoolbox.cltoolbox import Program
 
 main = Program()
@@ -5,3 +8,9 @@ command = main.command
 arg = main.arg
 parse = main.parse
 execute = main.execute
+
+# add git submodule to path to allow imports to work
+submodule_names = ["python-rst2ansi/rst2ansi"]
+for submodule_name in submodule_names:
+    (parent_folder_path, current_dir) = os.path.split(os.path.dirname(__file__))
+    sys.path.append(os.path.join(parent_folder_path, submodule_name))
